d9c6e8604128fd2eb5112e1031f884998c320ccf
HaveThePro <168744721+HaveThePro@users.noreply.github.com>
2024-12-02 20:40:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-const dc = require('./discord');
-const { sig } = require("./utils/sig");
 
-(async () => {
-    sig();
-
-    await dc.initialize();
-    // here is where you enter your email and password
-    await dc.login('gamerthelol123@gmail.com', 'Farfalla1234')
-
-    await dc.likeChannelProcess('851548488566439946', 'https://discord.com/channels/851548488566439946/1136800667525193819', 1) // 1 = 1 minute
-
-    debugger;
-
-})();
