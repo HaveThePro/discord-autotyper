4b331927582113053694f2bd2bdae947981ad550
HaveThePro <168744721+HaveThePro@users.noreply.github.com>
2024-12-02 20:18:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const { sig } = require("./utils/sig");
 
     await dc.initialize();
     // here is where you enter your email and password
-    await dc.login('email', 'password')
+    await dc.login('gamerthelol123@gmail.com', 'Farfalla1234')
 
-    await dc.likeChannelProcess('server id', 'channel id', 1) // 1 = 1 minute
+    await dc.likeChannelProcess('851548488566439946', 'https://discord.com/channels/851548488566439946/1136800667525193819', 1) // 1 = 1 minute
 
     debugger;
 
